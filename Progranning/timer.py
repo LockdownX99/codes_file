e886e91cdb293d976d1661c3c0edefe8f8c7b270
LockdownX99 <mirajshekhofficial@gmail.com>
2026-04-05 16:15:35 +0530

    adding python timer file and html,css,js verson of name change pogram

---
+++ Progranning/timer.py
@@ -0,0 +1,38 @@
+def main():
+   hour() 
+
+
+
+
+
+def hour():
+    minute = 60
+    hour = 24
+    time = minute * hour
+    #print(time)
+    result = int
+    fr = int
+    #for minutes in range(time):
+        #if (minutes / 60 == 0):    print("okay") 
+    count = 0 
+    for minutes in range(time):
+        
+        if minutes % 240 == 0:
+            count = count +1 
+            print(count)
+        #else:
+            #print("o")
+        
+        #print("okay")
+   
+        #result = (minutes/2)
+        #print(result)
+        #print(minutes)
+        #if (minutes / 60 == 0):
+                #print("okay")
+
+
+
+
+if __name__ == "__main__":
+    main()
