9742daf2f470ef8fab36649be1e9c7b6e37a74dc
LockdownX99 <mirajshekhofficial@gmail.com>
2026-04-27 17:37:49 +0530

    adding my won writen math module

---
+++ python/math_module.py
@@ -0,0 +1,22 @@
+def squre(n):
+    return n*n
+
+def addition(a,b):
+    return a+b        
+
+def subtraction(a,b):
+    return a-b
+
+def multiplication(a,b):
+    return a*b
+    
+def division(a,b):
+    return a/b
+    
+
+def bominal(a,b):
+    return a*a+2*a*b+b*b
+    
+    
+def root(n):
+    return n**0.5
---
+++ python/test.py
@@ -0,0 +1,5 @@
+from math_module import root
+
+while True:
+    n = int(input("The number you want Squre Root of \n"))
+    print (f"the root of {n} is {root(n)}\n")
